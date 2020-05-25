a2cdd6bdaac4abec525069ea70aed6592fbf394c
onuki <onuki@NA19498.nature.local>
2020-05-25 10:58:59 +0900

    0525 40行目 コメント追加

--- WebProject/src/Test.java
+++ WebProject/src/Test.java
@@ -37,6 +37,7 @@ public class Test extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 	}
 
 }
