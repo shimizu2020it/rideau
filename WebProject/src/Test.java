8f836f184e9f0eca66966e362d43d40d5462ec5b
kiridoushi <kiridoushi@MP605.mzdomain.maize.co.jp>
2020-05-25 10:49:48 +0900

    05/25コメント変更

--- WebProject/src/Test.java
+++ WebProject/src/Test.java
@@ -29,7 +29,7 @@ public class Test extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	}//コメント
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
